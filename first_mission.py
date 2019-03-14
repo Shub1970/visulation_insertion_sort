cdbaef98bd812911283c259465d51c1dce86c436
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2019-03-15 02:45:13 +0530

    now you can also saw insertion sort on bar graph

--- first_mission.py
+++ first_mission.py
@@ -22,6 +22,30 @@ plt.show()
 print(yaxis)
 
 
+#for bar graphimport matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+yaxis=[20,1,17,3,2,8,6,5,9,19,18,13,22,14,15,5,2,7,6]
+xaxis=[i for i in range(0,len(yaxis))]
+pos=np.arange(len(yaxis))+0.0001
+fig=plt.figure()
+ax1=fig.add_subplot(1,1,1)
+i=0
+def quicksho(arr):
+    global i,yaxis
+    i=i+1
+    key=yaxis[i]
+    j=i-1
+    while j>=0 and yaxis[j]>key and i<len(yaxis):
+        yaxis[j+1]=yaxis[j]
+        j=j-1
+    yaxis[j+1]=key
+    ax1.clear()
+    plt.bar(pos,yaxis)
+    print(yaxis)
+ani=animation.FuncAnimation(fig,quicksho,interval=10)
+plt.show()
+print(yaxis)
 
 
 
